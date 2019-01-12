402fc2e25e8d2adb389ca32706f6ed515bb1fbd4
Oleksandr Pimenov <pialgi@live.com>
2019-01-12 12:15:59 -0500

    Moved init to config. Removed Cygwin so, don't pull to windows.

--- config.el
+++ config.el
@@ -1,6 +1,35 @@
+
 ;; Tell emacs where is your personal elisp lib dir
 (add-to-list 'load-path "~/.emacs.d/packages/")
+;;macro for setting custom variables
+(defmacro csetq (variable value)
+  `(funcall (or (get ',variable 'custom-set)
+                'set-default)
+            ',variable ,value))
 
+;;; There are few public repositories of Emacs packages. 
+;;; I'm using melpa and you should too. I don't know why though.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+;; use-package is a nice package that simplifies package installation
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; there is a flavor of Emacs called spacemacs, 
+;;; that uses space key instead of Ctrl. From what
+;;; I could gather it is clunky, but the theme is nice.
+(unless (package-installed-p 'spacemacs-theme)
+   (package-refresh-contents)
+   (package-install 'spacemacs-theme))
+
+;;No need to describe this feature. Everybody knows it. Maybe not.
+;;It allows you to have as many cursors on the screen as your 
+;; CPU+GPU+Your-Head can handle. Allows you to use Dark Magic. 
 (use-package multiple-cursors
   :ensure t
   :config
@@ -10,19 +39,22 @@
      (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
      (define-key mc/keymap (kbd "<return>") nil))
 
+;;Helm is modified help buffer. It attaches itself to a lot of actions
+;;in Emacs environment and create more functional and better looking
+;;help buffers. 
 (use-package helm
   :ensure t
   :init
   (helm-mode 1))
 
-;;bind a key to help
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-;;bind a key to help-swoop
-(global-set-key (kbd "M-i") 'helm-swoop)
-
-;;bind a key to help-swoop
-(global-set-key (kbd "M-u") 'helm-show-kill-ring)
+;;install package first
+(use-package helm-swoop
+  :ensure t
+  :config
+     (global-set-key (kbd "M-x") 'helm-M-x)
+     (global-set-key (kbd "M-i") 'helm-swoop);;helm search
+     (global-set-key (kbd "M-u") 'helm-show-kill-ring);;BEST FEATURE EVER
+     (global-set-key (kbd "C-c f") 'helm-locate));;find stuff quickly
 
 (defvar my-term-shell "/bin/bash")
 (defadvice ansi-term (before force-bash)
@@ -76,12 +108,7 @@
   :init
   (beacon-mode -1))
 
-;;(setq-default indent-tabs-mode nil)
-;;(setq-default tab-width 3)
-;;(setq tab-stop-list (number-sequence 3 200 3))
-;;(setq indent-line-function 'insert-tab)
-;;(global-set-key (kbd "TAB") 'self-insert-command)
-
+;;clean up GUI GARBAGE
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -96,15 +123,29 @@
 
 (desktop-save-mode 1)
 
-;; load the packaged named xyz.
-;; best not to include the ending “.el” or “.elc”
-(load "cygwin-mount")
-(load "setup-cygwin")
+;;if you are on Linux 
+(use-package ediff
+   :ensure t
+   :init)
 
-(setenv "PATH" (concat "D:/Cygwin/bin;" (getenv "PATH")))
-(setq exec-path (cons "D:/Cygwin/bin" exec-path))
-(require 'cygwin-mount)
-(cygwin-mount-activate)
+;;if you are on windouzzzzz os. Good luck. 
+;; download cygwin-mount and setup-cygwin to "D:/Cygwin/bin"
+;; Do you feel lucky today? try leaving package extensions.
+;; best not to include the ending “.el” or “.elc” 
+;;(load "cygwin-mount")
+;;(load "setup-cygwin")
+
+;;(setenv "PATH" (concat "D:/Cygwin/bin;" (getenv "PATH")))
+;;(setq exec-path (cons "D:/Cygwin/bin" exec-path))
+;;(require 'cygwin-mount)
+;;(cygwin-mount-activate)
+
+;;(csetq ediff-split-window-function 'split-window-horizontally)
+;;(csetq ediff-diff-options "-w")
+;;(csetq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;;(winner-mode)
+;;(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
 
 (use-package s
   :ensure t
@@ -121,14 +162,29 @@
 
 (global-set-key (kbd "C-x C-r") (lambda () (interactive) (helm-swoop :$query "error:")))
 
-(fset 'lstar-build-push-bash
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([7 3 134217839 46 47 83 116 97 114 tab return] 0 "%d")) arg)))
-(global-set-key (kbd "C-c s") 'lstar-build-push-bash)   
+(fset 'build-and-push-rs
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([7 3 134217839 46 47 83 116 97 114 66 117 105 108 100 80 117 115 104 46 98 97 116] 0 "%d")) arg)))
+(global-set-key (kbd "C-c s") 'build-and-push-rs)   
 ;;(fset 'star-build
 ;;   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([3 134217839 83 116 97 114 tab return] 0 "%d")) arg)))
 
+(use-package magit
+   :ensure t
+   :init)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+(use-package helm-flyspell
+  :ensure t
+  :config
+  (global-set-key (kbd "C-:") 'helm-flyspell-correct))
 
 (use-package sr-speedbar
   :ensure t
   :init)
+
+(use-package pylint
+  :ensure t
+  :init)
+(autoload 'pylint "pylint")
+(add-hook 'python-mode-hook 'pylint-add-menu-items)
+(add-hook 'python-mode-hook 'pylint-add-key-bindings)
--- init.el
+++ init.el
@@ -1,43 +1,9 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(unless (package-installed-p 'magit)
-  (package-refresh-contents)
-  (package-install 'magit))
-
-(unless (package-installed-p 'spacemacs-theme)
-  (package-refresh-contents)
-  (package-install 'spacemacs-theme))
-
-(unless (package-installed-p 'helm)
-  (package-refresh-contents)
-  (package-install 'helm))
-
-(unless (package-installed-p 'helm-swoop)
-  (package-refresh-contents)
-  (package-install 'helm-swoop))
-
-(unless (package-installed-p 'log4j-mode)
-  (package-refresh-contents)
-  (package-install 'log4j-mode))
-
-(unless (package-installed-p 'ediff)
-  (package-refresh-contents)
-  (package-install 'ediff))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              HERE BE DRAGONS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Tell Emacs that you own it, and it should do what config.org says.
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                         DO NOT MODIFY BELOW
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -48,6 +14,7 @@
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(electric-pair-mode t)
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
     (s expand-region rainbow-delimiters rainbow-delimeters multiple-cursors indent-guide eshell-up powerline log4j-mode magit helm-config helm-swoop helm dired-sidebar org-bullets beacon spacemacs-theme dracula-theme which-key use-package)))
@@ -57,7 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Consolas")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "adobe" :family "Office Code Pro")))))
 
 (put 'erase-buffer 'disabled nil)
 (put 'downcase-region 'disabled nil)
