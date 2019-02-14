52bf24361f1fbe9aeac638f74d36c7fd37cb6232
opimenov <opimenov@srcinc.com>
2019-02-14 13:14:52 -0500

    treemacs dired+ installed. Tramp does not work for some files.

--- init.el
+++ init.el
@@ -50,13 +50,14 @@
  '(custom-enabled-themes (quote (spacemacs-dark)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(display-time-mode t)
  '(electric-pair-mode t)
  '(global-display-line-numbers-mode t)
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (smart-mode-line-powerline-theme smart-mode-line flycheck-clang-analyzer company-c-headers flycheck yasnippet-snippets yasnippet treemacs-magit treemacs-icons-dired treemacs-projectile treemacs frame-local ov diminish htmlize rainbow-mode switch-window sunrise-x-modeline sunrise-x-buttons sunrise-commander spaceline ox-twbs avy ac-ispell helm-projectile jedi company-jedi pretty-mode company-irony company s expand-region rainbow-delimiters rainbow-delimeters multiple-cursors indent-guide eshell-up powerline log4j-mode magit helm-config helm-swoop helm dired-sidebar org-bullets beacon spacemacs-theme dracula-theme which-key use-package)))
+    (auto-complete-exuberant-ctags dashboard smart-mode-line-powerline-theme smart-mode-line flycheck-clang-analyzer company-c-headers flycheck yasnippet-snippets yasnippet frame-local ov diminish htmlize rainbow-mode switch-window sunrise-x-modeline sunrise-x-buttons sunrise-commander spaceline ox-twbs avy ac-ispell helm-projectile jedi company-jedi pretty-mode company-irony company s expand-region rainbow-delimiters rainbow-delimeters multiple-cursors indent-guide eshell-up powerline log4j-mode magit helm-config helm-swoop helm dired-sidebar org-bullets beacon spacemacs-theme dracula-theme which-key use-package)))
  '(send-mail-function (quote mailclient-send-it))
  '(size-indication-mode t)
  '(tool-bar-mode nil))
